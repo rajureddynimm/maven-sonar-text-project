f6ca3e811704a88f503a312950b610838d2a537a
rajureddynimm123@gmail.com <Raju Reddy N>
2023-08-24 20:03:20 +0530

    add all files modified

---
+++ src/main/java/com/in/mavensonartextproject/MavenSonarTextProjectApplication.java
@@ -0,0 +1,13 @@
+package com.in.mavensonartextproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenSonarTextProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenSonarTextProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/in/mavensonartextproject/MavenSonarTextProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in.mavensonartextproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenSonarTextProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
